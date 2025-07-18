2238513817e19af177d352cfd7e7c3b747c4ae57
Harshit Chaurasia <harshitchaurasia4673@gmail.com>
2025-07-18 20:26:32 +0530

    levelorder

--- binarytrees/levelorder.cpp
+++ binarytrees/levelorder.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 // Definition for a binary tree node
-struct TreeNode {
+struct TreeNode
+{
     int val;
     TreeNode *left;
     TreeNode *right;
@@ -13,44 +14,87 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
         vector<vector<int>> ans;
-        if (root == nullptr) return ans;
+        if (root == nullptr)
+            return ans;
 
         vector<int> sm;
-        queue<TreeNode*> q;
+        queue<TreeNode *> q;
         q.push(root);
         q.push(NULL);
 
-        while (!q.empty()) {
-            TreeNode* temp = q.front();
+        while (!q.empty())
+        {
+            TreeNode *temp = q.front();
             q.pop();
 
-            if (temp == NULL) {
+            if (temp == NULL)
+            {
                 ans.push_back(sm);
                 sm.clear();
-                if (!q.empty()) q.push(NULL);
-            } else {
-                sm.push_back(temp->val);
-                if (temp->left) q.push(temp->left);
-                if (temp->right) q.push(temp->right);
+                if (!q.empty())
+                    q.push(NULL);
             }
+            else
+            {
+                sm.push_back(temp->val);
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
+            }
+        }
+
+        return ans;
+    }
+
+    vector<vector<int>> levelOrder2(TreeNode *root)
+    { // without NUll , optimal
+        vector<vector<int>> ans;
+        if (!root)
+            return ans;
+
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int size = q.size();
+            vector<int> level;
+
+            for (int i = 0; i < size; ++i)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                level.push_back(node->val);
+
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+
+            ans.push_back(level);
         }
 
         return ans;
     }
 };
 
-int main() {
+int main()
+{
     // Sample binary tree:
     //       1
     //      / \
     //     2   3
     //    / \
     //   4   5
-    TreeNode* root = new TreeNode(1);
+    TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2, new TreeNode(4), new TreeNode(5));
     root->right = new TreeNode(3);
 
@@ -59,8 +103,10 @@ int main() {
 
     // Print the result
     cout << "Level-order Traversal:\n";
-    for (const auto& level : result) {
-        for (int val : level) {
+    for (const auto &level : result)
+    {
+        for (int val : level)
+        {
             cout << val << " ";
         }
         cout << endl;
